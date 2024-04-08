26683819ded593faa4e8b202ea1bc09d0504ccd7
Maria Khaklenko <maria.khalenko@gmail.com>
2024-04-08 15:57:25 +0300

    goit-react-hw-05

--- src/components/MovieCast/MovieCast.jsx
+++ src/components/MovieCast/MovieCast.jsx
@@ -1,4 +1,4 @@
-import { fetchMovieCast } from "";
+import fetchMovieCast from "../../api/api";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import Loader from "../Loader/Loader";
